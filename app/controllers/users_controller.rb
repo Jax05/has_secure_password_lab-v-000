7a45926eb2a2064abc8687e78d740ef84ea7be9e
Carley Tripp <carley.tripp@outlook.com>
2019-04-05 02:05:53 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,11 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
 
-    if !(user.password == user.password_confirmation)
-      redirect_to new_user_path
-    elsif user.save
+    if user.save
       session[:user_id] = user.id
       redirect_to welcome_index_path
     else
-      render :new
+      redirect_to new_user_path
     end
   end
 
