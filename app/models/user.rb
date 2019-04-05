b101519e26e55bbe24b7abd8be345059c6e8fd6b
Carley Tripp <carley.tripp@outlook.com>
2019-04-05 01:33:00 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
-  validates :password, presence: true
 end
